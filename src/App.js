995be325dd9773c1013610d5d7d14f926f6e41e9
mohammad javad <mjkmo123@gmail.com>
2021-01-25 08:52:58 +0330

    add simple routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import GetName from "./GetName";
+import {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <GetName />
-    </div>
-  );
+    const [pageState, setPageState] = useState(1);
+
+    function renderPage(page) {
+        switch (page) {
+            case 1 : return  <GetName setPage={setPageState} />;
+            break;
+        }
+    }
+
+    return (
+        <div className="App">
+            {renderPage(pageState)}
+        </div>
+    );
 }
 
 export default App;
--- src/GetName.js
+++ src/GetName.js
@@ -1,28 +1,54 @@
 import './GetName.css';
 import {useState} from 'react';
+import {animated, useSpring} from "react-spring";
 
-export default function GetName({done}) {
+export default function GetName({setPage}) {
     const [isLoading, changeLoading] = useState(false);
     const [username, changeUsername] = useState('');
 
+    const welcomeProp = useSpring({
+        opacity: 1,
+        from: { opacity: 0 },
+        delay: 500
+    });
+
+    const [componentProp, setProp] = useSpring(() => {
+        return {
+            opacity: 1,
+            onRest(ds) {
+                if (ds.opacity) return null;
+
+                setPage(2)
+            }
+        }
+    });
+
+    function submitName(){
+        changeLoading(true);
+
+        setTimeout(() => {
+            setProp({opacity: 0});
+        }, 5000);
+    }
+
     return (
-        <div className="container">
-            <span className="text">welcome to tic tac toe {username}!</span>
+        <animated.div style={componentProp} className="container">
+            <animated.span className="text" style={welcomeProp}>welcome to tic tac toe {username}!</animated.span>
             <input
                 type="text"
                 className="input"
                 placeholder="your name"
-                onChange={(e) => changeUsername(", " + e.target.value)}
+                onChange={(e) => changeUsername(e.target.value)}
             />
 
             <button
                 className="button"
-                onClick={() => done()}
+                onClick={submitName}
                 disabled={isLoading}
             >
-                {isLoading ? <Spinner /> : "lets go!"}
+                {isLoading ? <Spinner /> : <span>lets go!</span>}
             </button>
-        </div>
+        </animated.div>
     )
 }
 
